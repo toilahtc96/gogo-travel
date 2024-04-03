e28cf6f33dcd729bb826b92f8368d23ac86e4fd4
conght2 <conght2@msb.com.vn>
2024-04-03 22:25:14 +0700

    update

---
+++ src/main/java/com/travel/gogo/controller/RegistrationPublicController.java
@@ -0,0 +1,24 @@
+package com.travel.gogo.controller;
+
+import com.travel.gogo.entity.Registrations;
+import com.travel.gogo.request.RegistrationRequest;
+import com.travel.gogo.service.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("public/registration")
+@AllArgsConstructor
+@CrossOrigin(origins = {"http://gogotravelvn.com/", "http://localhost:3000"})
+public class RegistrationPublicController {
+    private final RegistrationService registrationService;
+
+    @PostMapping("/add")
+    public ResponseEntity addRegistration(@RequestBody RegistrationRequest registrationRequest) {
+        registrationService.addRegistration(registrationRequest);
+        return ResponseEntity.noContent().build();
+    }
+}
